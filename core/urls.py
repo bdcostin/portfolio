b6857290de39435c9f4f88409e6b53bae3ddaae7
bdcostin <bailey.costin@gmail.com>
2019-05-20 20:45:25 -0400

    updates index page with footer and images

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('', views.ProjectsView.as_view(), name='projects'),
+    path('projects/', views.ProjectsView.as_view(), name='projects'),
 ]
