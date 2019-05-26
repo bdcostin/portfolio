6439aeae397aa86d7604e87f747a84044ef161e4
bdcostin <bailey.costin@gmail.com>
2019-05-26 18:13:58 -0400

    add javascript, create greeting on index

---
+++ static/js/main.js
@@ -0,0 +1,28 @@
+'use strict'
+
+var $ = require('jquery')
+
+$(document).scroll(function () {
+  var yAxis = $(this).scrollTop()
+  if (yAxis > 350) {
+    $('.sidenav-icons-spacing').fadeIn()
+  } else {
+    $('.sidenav-icons-spacing').fadeOut()
+  }
+})
+
+document.addEventListener('mousemove', function (event) {
+  const x = event.pageX
+  const y = event.pageY
+
+  document.querySelectorAll('section div').forEach(div => {
+    const dx = div.offsetLeft + 50 - x
+    const dy = div.offsetTop + 50 - y
+    const dist = Math.sqrt(dx * dx + dy * dy)
+
+    const score = Math.exp(dist * -0.003)
+
+    div.style.transform = 'scale(' + score + ')'
+    div.style.fontWeight = 100 + (100 * Math.floor(8 * score))
+  })
+})
