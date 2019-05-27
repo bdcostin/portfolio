4f28663854ca90f358d0b3e55ac73578c033b793
bdcostin <bailey.costin@gmail.com>
2019-05-27 09:18:38 -0400

    link resume and contact pages

--- core/urls.py
+++ core/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('projects/', views.ProjectsView.as_view(), name='projects'),
+    path('contact/', views.contact_me, name='contact'),
+    path('resume/', views.ResumeView.as_view(), name='resume'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView, ListView
+from django.http import HttpResponse 
+from .forms import ContactForm 
 
 # Create your views here.
 
@@ -8,3 +10,17 @@ class IndexView(TemplateView):
     
 class ProjectsView(ListView):
     template_name = "projects.html"
+
+def contact_me(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            # send email code goes here
+            return HttpResponse('Thanks for contacting me!')
+    else:
+        form = ContactForm()
+
+    return render(request, 'contact-me.html', {'form': form})
+
+class ResumeView(TemplateView):
+    template_name = "resume.html"
