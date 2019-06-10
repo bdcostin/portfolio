603a91b4b9211762eb3d34cf53e6b48764d03eab
bdcostin <bailey.costin@gmail.com>
2019-06-10 01:48:38 -0400

    add resume html and pdf

--- static/js/main.js
+++ static/js/main.js
@@ -13,18 +13,10 @@ $(document).scroll(function () {
   }
 })
 
-document.addEventListener('mousemove', function (event) {
-  const x = event.pageX
-  const y = event.pageY
-
-  document.querySelectorAll('section div').forEach(div => {
-    const dx = div.offsetLeft + 50 - x
-    const dy = div.offsetTop + 50 - y
-    const dist = Math.sqrt(dx * dx + dy * dy)
-
-    const score = Math.exp(dist * -0.003)
-
-    div.style.transform = 'scale(' + score + ')'
-    div.style.fontWeight = 100 + (100 * Math.floor(8 * score))
-  })
+$(document).ready('.accordion').on('click', '.accordion-header', function () {
+  // will (slide) toggle the related panel.
+  $(this)
+    .toggleClass('active')
+    .next()
+    .slideToggle()
 })
