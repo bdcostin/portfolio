173096fb42b621d60f25922c1761f318787a7aa7
bdcostin <bailey.costin@gmail.com>
2019-06-09 22:01:54 -0400

    Fixes 404 error on Resume page

--- core/urls.py
+++ core/urls.py
@@ -3,8 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('projects/', views.ProjectsView.as_view(), name='projects'),
-    # path('<slug:slug>/', views.project_detail, name='project_detail'),
-    path('<slug:slug>/', views.ProjectDetailView.as_view(), name='project-detail'),
     path('resume/', views.ResumeView.as_view(), name='resume'),
+    path('projects/', views.ProjectsView.as_view(), name='projects'),
+    path('<slug:slug>/', views.ProjectDetailView.as_view(), name='project-detail'),
 ]
