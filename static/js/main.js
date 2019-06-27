68e1e48c0317e21c6176f44c032e7f1989f1b5be
bdcostin <bailey.costin@gmail.com>
2019-06-26 21:37:08 -0400

    fixes styling on large monitors

--- static/js/main.js
+++ static/js/main.js
@@ -20,7 +20,8 @@ $(document).ready(function () {
     $('.sidenav').remove()
     $('.top-nav').remove()
     $('.container').removeClass('hide-container')
-    $('.main').css({ 'margin-left': '15px', 'margin-right': '15px' })
+    $('.main').removeClass('full-spacing')
+    $('.main').addClass('small-spacing')
     $('.footer').css({ 'margin-right': '15px' })
     $('.plato-speaks').css({ 'margin-left': '70px', 'margin-top': '10px' })
   } else {
@@ -28,9 +29,9 @@ $(document).ready(function () {
   }
 })
 
-$(document).ready('.accordion').on('click', '.accordion-header', function () {
-  $(this)
-    .toggleClass('active')
-    .next()
-    .slideToggle()
+$(document).ready(function () {
+  const windowWidth = $(window).width()
+  if (windowWidth > 1500) {
+    $('.sidenav').css({ 'margin-left': '670px' })
+  }
 })
